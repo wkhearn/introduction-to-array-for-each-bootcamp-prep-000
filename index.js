98f23fb8a79a1e243b774aaafe700c3e8cb60573
Kenny Hearn <wkhearn@gmail.com>
2017-02-17 00:41:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+
+var animals = ["dog", "fish", "cat"]
+
+doToElementsInArray(animals, changeCompletely)
+
+console.log(animals)
